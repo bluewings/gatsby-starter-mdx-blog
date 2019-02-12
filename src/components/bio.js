7459a91b7632acd2c4d9617ea8938077cf8882b0
sungwon.cha <mygenie3@naver.com>
2019-02-12 22:36:12 +0900

    Update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -31,12 +31,14 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Suwon, South Korea 
+              building useful things.
               {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              {social.twitter && (
+                <a href={`https://twitter.com/${social.twitter}`}>
+                  You should follow him on Twitter
+                </a>
+              )}
             </p>
           </div>
         )
