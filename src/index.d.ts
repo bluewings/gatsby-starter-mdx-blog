3d76b7df4bf6b221b532467672d2489a4e15637c
sungwon.cha <mygenie3@naver.com>
2019-07-22 11:17:42 +0900

    fix Code component

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,10 +2,7 @@ declare module 'gatsby-mdx';
 declare module 'disqus-react';
 declare module 'typography-theme-wordpress-2016';
 declare module '@mdx-js/tag';
-
-// import { MDXProvider } from
-// import rangeParser from 'parse-numeric-range';
-// import { Code } from '../components/code';
+declare module 'react-octicon';
 
 declare module '*.scss' {
   const classes: { [key: string]: string };
